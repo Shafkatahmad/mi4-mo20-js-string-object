b89c36d2dbc3192cf16ecc42e129f6cd7e88dac6
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-16 22:03:16 +0600

    toLowerCase method

---
+++ case.js
@@ -0,0 +1,18 @@
+const school = 'RAJ UK UTTARA MODEL SCHOOL';
+console.log(school);
+console.log(school.toLowerCase());
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+// if(subject === book) {
+//   console.log('I will read');
+// }
+// else {
+//   console.log('I will not read');
+// }
+subject.toLowerCase === book.toLowerCase? console.log('I will read'): console.log('I will not read');
+
+const drink = ' water';
+const liquid = 'water';
+drink.trim() === liquid.trim()? console.log('Can drink'): console.log('Can not drink');
