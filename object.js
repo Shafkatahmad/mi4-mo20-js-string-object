810908d4ca0472ce025ea8bb3f93952ab0063742
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-16 22:58:24 +0600

    object

---
+++ object.js
@@ -0,0 +1,33 @@
+// premitive object
+const age = 21;
+const school = 'Harvard Law';
+const isPassed = true;
+let isDeveloper;
+
+// console.log(isDeveloper);
+
+
+const bottle_wrong = ['white', 45];
+const bottleColor = 'white';
+const bottlePrice = '45';
+const bottleDrinks = 'water';
+
+// object
+// non-primitive object
+const bottle = {
+  brand: 'apple',
+  price: 45,
+  color: 'white',
+  isClean: false
+}
+
+const subject = {
+  name: 'biology',
+  teacher: 'Tuhin sir',
+  examDate: '30 Feb',
+  chapters: ['first', 'second', 'third'],
+  exams: {
+    name: 'final',
+    marks: 100
+  }
+}
