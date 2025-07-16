5e30a751f707db533e9fd65bd49630cdf99f3f0b
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-16 21:44:43 +0600

    string immutable

--- string.js
+++ string.js
@@ -1,1 +1,21 @@
-const country = 'Bangladesh'
+const country = 'Bangladesh';
+const division = "NewKhale";
+const district = `B-Baria`;
+// const thana = new String('Demra');
+
+// console.log(thana);
+
+// ARRAY IS MUTABLE --> CAN CHANGE THE ARRAY ELEMENTS
+const numbers = [54, 98, 78, 21, 65];
+console.log(numbers);
+numbers[1] = 11;
+console.log(numbers);
+console.log(numbers.length);
+// console.log(numbers[0]);
+// console.log(typeof numbers);
+
+// STRING IS IMMUTABLE --> cAN NOT CHANGE THE STRING ELEMENTS
+const capital = 'Dhaka';
+console.log(capital);
+capital[0] = 'F';
+console.log(capital);
