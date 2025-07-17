d3bbe8e1a8ca2c66ae50a5bcfb8dfc85f3419a36
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-17 13:06:31 +0600

    js object task

--- js-object-tasks.js
+++ js-object-tasks.js
@@ -50,6 +50,27 @@ for(let number in Student) {
 
 
 // print the key and the typeOf value
+/**
+ * Loop through an object and print the key-value pairs with their types
+
+Input:
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+Output:
+
+
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean
+ */
 let myObject = {
   name: 'John Doe',
   age: 25,
